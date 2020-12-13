b4d3c6367bf5baf02b5f380d1b70ece44732eac9
artwilton <Arthur.M.Wilton@gmail.com>
2020-12-13 02:58:11 -0500

    seed data finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Container.destroy_all
-Type.destroy_all
-User.destroy_all
-
 types = Type.create([
     {name: 'Building', default: true},
     {name: 'Room', default: true},

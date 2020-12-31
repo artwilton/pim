cb51b142d9ae70c0e0a97de7a4737342786ac1c5
artwilton <Arthur.M.Wilton@gmail.com>
2020-12-31 02:54:52 -0500

    updated create_item_serializer to include photo attribute

--- app/serializers/create_item_serializer.rb
+++ app/serializers/create_item_serializer.rb
@@ -1,7 +1,7 @@
 class CreateItemSerializer < ActiveModel::Serializer
     include Rails.application.routes.url_helpers
 
-    attributes :id, :name, :description, :notes, :barcode, :container, :category
+    attributes :id, :name, :description, :notes, :barcode, :container, :category, :photo
 
     def container
         {id: self.object.container.id, name: self.object.container.name}
@@ -9,6 +9,10 @@ class CreateItemSerializer < ActiveModel::Serializer
 
     def category
         {id: self.object.category.id, name: self.object.category.name}
+    end
+
+    def photo
+        {uri: self.object.photo.attached? ? rails_blob_path(self.object.photo, only_path: true) : null}
     end
 
 end

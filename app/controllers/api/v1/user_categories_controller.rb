bd888c9b0d0b86b1a5c61aec72b48a6d8b840d8c
artwilton <Arthur.M.Wilton@gmail.com>
2021-01-02 03:15:19 -0500

    updated user_categories_controller destroy method

--- app/controllers/api/v1/user_categories_controller.rb
+++ app/controllers/api/v1/user_categories_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::UserCategoriesController < ApplicationController
     def destroy
         user_category = UserCategory.find_by(user_id: params[:user_id], category_id: params[:category_id])
         user_category.destroy
+        head :no_content
 
         # user = User.find(params[:user_id])
         # render json: user, serializer: UserCategoriesSerializer
